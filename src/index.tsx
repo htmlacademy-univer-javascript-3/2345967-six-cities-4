34f595506a177ae4bd81e5ee782bfc0cea332848
naego <naegorov@hotmail.com>
2024-03-01 12:46:16 +0300

    Начинаем программировать

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const Setting = {
+  pageMainCntCard: 10
+};
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App placesCount={Setting.pageMainCntCard}/>
   </React.StrictMode>
 );
